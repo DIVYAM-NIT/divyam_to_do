d0f5f38cc9574eefbc1c2cf5738c741cea039257
DIVYAM SHARMA NIT <ds7081241904@gmail.com>
2024-06-08 15:50:19 +0530

    updated

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,17 +4,19 @@ export default function Item(props) {
     return (
         <>
             <div className="container text-center single_item">
-                <div className="row align-items-start">
-                    <div className="col text_align">
+                <div className="row">
+                    <div className="col">
                         {props.item_name}
                     </div>
-                    <div className="col text_align">
+                    <div className="col">
                         {props.item_date}
                     </div>
-                    <div className="col buttons">
+                    <div className="col">
                         <button type='button' className={`btn btn-danger`} onClick={() => {
                             props.del(props.item_name);
                         }}>REMOVE</button>
+                    </div>
+                    <div className="col">
                         <button type='button' className={`btn btn-danger`} onClick={() => {
                             props.update(props.item_name);
                         }}>UPDATE</button>
